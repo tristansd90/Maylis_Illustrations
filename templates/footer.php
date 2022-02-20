0c95056eb6ff9294b4e70b2093838b1c6b0cb01e
Tristan SCHMID <tristan.90@icloud.com>
2022-02-20 08:13:37 +0100

    added template

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<?php echo
+    "<div class='horizontale_divider'></div>
+    <p class='domaine'>Maylis Illustrations</p>
+    <div class='link'>
+        <a class='condition' href=''>Condition d'utilisation</a>
+        <a class='politique' href='' >Politiques de Confidentialité</a>
+    </div>
+    <p class='copyright'>© 2021 Maylis Illustrations | Maylis</p>"
+?>
---
+++ templates/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>header</title>
+</head>
+
+<body>
+
+    <?php echo
+
+    "<div class='banniere'>
+
+        <img src='./images/fille3.png' alt='dessin'>
+        <a href='index.html'><h1>Maylis Illustrations</h1></a>
+        <img src='./images/famille1.png' alt='famille'>
+
+    </div>
+
+    <nav>
+
+        <a href='index.php'>dessins</a>
+        <a href='books.php'>livres</a>
+        <a href='interview.php'>me connaitre</a>
+        <a href='contact.php'>contact</a>
+        <a href='https://www.instagram.com/maylis.illustrations/?hl=en' target='_blank'><i class='fab fa-instagram'></i></a>
+
+    </nav>"
+    
+    ?>
+
+</body>
+
+</html>
